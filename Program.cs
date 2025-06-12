6fd8794d763bca6352156a29e4fc067a4a2067b0
sadovnichek <kapkaevictor@gmail.com>
2025-06-12 17:47:43 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using MPI;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var steps = 4;
+        var (map, length, width) = LoadFromFile("state.txt");
+
+        for (var i = 0; i < steps; i++)
+        {
+            Console.Clear();
+            Console.WriteLine(DisplayState(map, length, width));
+            map = PerformStep(map, length, width);
+            Thread.Sleep(1000);
+        }
+    }
+
+    public static bool[] PerformStep(bool[] map, int length, int width)
+    {
+        var newState = new bool[length * width];
+        for(var i = 0; i < width; i++)
+        {
+            for(var j = 0; j < length; j++)
+            {
+                var alives = GetNeighborsCount(map, length, width, i, j);
+                if (alives == 3 || alives == 2 && map[i * length + j])
+                    newState[i * length + j] = true;
+            }
+        }
+        return newState;
+    }
+
+    public static string DisplayState(bool[] map, int length, int width)
+    {
+        var stringBuilder = new StringBuilder();
+        for (var i = 0; i < width; i++)
+        {
+            for(var j = 0; j < length; j++)
+            {
+                if (map[i * length + j])
+                    stringBuilder.Append('*');
+                else
+                    stringBuilder.Append('.');
+            }
+            stringBuilder.Append('\n');
+        }
+        return stringBuilder.ToString();
+    }
+
+    public static bool[] GetInitialConfiguration(int length, int width, Random random, int filledCells = 25)
+    {
+        var map = new bool[length * width];
+        for(var i = 0; i < filledCells; i++)
+        {
+            var x = random.Next(0, width);
+            var y = random.Next(0, length);
+            map[x * length + y] = true;
+        }
+        return map;
+    }
+
+    public static int GetNeighborsCount(bool[] map, int length, int width, int i, int j)
+    {
+        var alives = 0;
+        for (var dx = -1; dx <= 1; dx++)
+        {
+            for (var dy = -1; dy <= 1; dy++)
+            {
+                if (dx == 0 && dy == 0)
+                {
+                    continue;
+                }
+                var u = i + dx >= 0 ? (i + dx) % width : i + dx + width;
+                var v = j + dy >= 0 ? (j + dy) % length : j + dy + length;
+                //var u = i + dx;
+                //var v = j + dy;
+                if (u >= 0 && u < width && v >= 0 && v < length && map[u * length + v])
+                {
+                    alives++;
+                }
+            }
+        }
+        return alives;
+    }
+
+    public static (bool[], int, int) LoadFromFile(string filename)
+    {
+        using (var reader = new StreamReader(filename))
+        {
+            var lines = new List<string>();
+            while(!reader.EndOfStream)
+            {
+                lines.Add(reader.ReadLine());
+            }
+            var length = lines.First().Length;
+            var map = new bool[lines.Count * length];
+            for(var i = 0; i < lines.Count; i++)
+            {
+                for(var j = 0; j < length; j++)
+                {
+                    if (lines[i][j] != ' ')
+                        map[i * length + j] = true;
+                }
+            }
+            return (map, length, lines.Count);
+        }
+    }
+
+    public static void SaveToFile(bool[] map, int length, int width, string filename)
+    {
+        using(var writer = new StreamWriter(filename))
+        {
+            writer.Write(DisplayState(map, length, width));
+        }
+    }
+}
