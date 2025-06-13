626eda475f1aa2c1b04f0097af6bf89e82fb6f8d
sadovnichek <kapkaevictor@gmail.com>
2025-06-13 14:22:29 +0500

    mpi implementation

--- Program.cs
+++ Program.cs
@@ -9,24 +9,97 @@ using MPI;
 
 class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        var steps = 4;
-        var (map, length, width) = LoadFromFile("state.txt");
-
-        for (var i = 0; i < steps; i++)
+        MPI.Environment.Run(ref args, communicator =>
         {
-            Console.Clear();
-            Console.WriteLine(DisplayState(map, length, width));
-            map = PerformStep(map, length, width);
-            Thread.Sleep(1000);
-        }
+            var rank = communicator.Rank;
+            var size = communicator.Size;
+            bool[] initialState = null;
+            int length = 0;
+            int width = 0;
+            int steps = 0;
+
+            if (rank == 0)
+            {
+                (initialState, length, width) = LoadFromFile(args[0]);
+                steps = int.Parse(args[1]);
+                Console.WriteLine(DisplayState(initialState, length, width));
+            }
+
+            communicator.Broadcast(ref initialState, 0);
+            communicator.Broadcast(ref length, 0);
+            communicator.Broadcast(ref width, 0);
+            communicator.Broadcast(ref steps, 0);
+
+            var blockWidth = width / size;
+
+            var currentBlock = new bool[(blockWidth + 2) * length];
+
+            for (var i = 0; i < blockWidth; i++)
+            {
+                for (var j = 0; j < length; j++)
+                {
+                    currentBlock[length + i * length + j] = initialState[rank * blockWidth * length + i * length + j];
+                }
+            }
+
+            for (var s = 0; s < steps; s++)
+            {
+                var localTop = currentBlock.Skip(length).Take(length).ToArray();
+                var localBot = currentBlock.Skip(length * blockWidth).Take(length).ToArray();
+
+                var topRank = (rank - 1 + size) % size;
+                var bottomRank = (rank + 1) % size;
+
+                //Console.WriteLine($"{rank}: sent top row to {topRank}");
+                communicator.SendReceive(localTop, topRank, 0, bottomRank, 0, out var topRow);
+                //Console.WriteLine($"{rank}: received top row: {string.Join("", topRow.Select(x => x ? "*" : "."))}");
+
+                //Console.WriteLine($"{rank}: sent bottom row to {bottomRank}");
+                communicator.SendReceive(localBot, bottomRank, 1, topRank, 1, out var bottomRow);
+                //Console.WriteLine($"{rank}: received bottom row: {string.Join("", bottomRow.Select(x => x ? "*" : "."))}");
+
+                for (var i = 0; i < length; i++)
+                {
+                    currentBlock[i] = bottomRow[i];
+                }
+                for (var i = 0; i < length; i++)
+                {
+                    currentBlock[(blockWidth + 1) * length + i] = topRow[i];
+                }
+
+                //Console.WriteLine($"{rank}: Local block before step performed\n{DisplayState(currentBlock, length, blockWidth + 2)}");
+                currentBlock = PerformStep(currentBlock, length, blockWidth + 2);
+                //Console.WriteLine($"{rank}: step {s + 1} performed\n{DisplayState(currentBlock, length, blockWidth + 2)}");
+
+                communicator.Barrier();
+            }
+
+            var gathered = communicator.Gather(currentBlock, 0);
+            communicator.Barrier();
+
+            if (rank == 0)
+            {
+                for(var i = 0; i < gathered.GetLength(0); i++)
+                {
+                    var subState = gathered[i].Skip(length).Take(length * blockWidth).ToArray();
+                    Console.Write(DisplayState(subState, length, blockWidth));
+                }
+            }
+        });
     }
 
     public static bool[] PerformStep(bool[] map, int length, int width)
     {
         var newState = new bool[length * width];
-        for(var i = 0; i < width; i++)
+
+        for(var i = 0; i < length; i++)
+        {
+            newState[i] = map[i];
+        }
+
+        for(var i = 1; i < width - 1; i++)
         {
             for(var j = 0; j < length; j++)
             {
@@ -35,6 +108,12 @@ class Program
                     newState[i * length + j] = true;
             }
         }
+
+        for (var i = 0; i < length; i++)
+        {
+            newState[(width - 1) * length + i] = map[(width - 1) * length + i];
+        }
+
         return newState;
     }
 
@@ -78,11 +157,9 @@ class Program
                 {
                     continue;
                 }
-                var u = i + dx >= 0 ? (i + dx) % width : i + dx + width;
-                var v = j + dy >= 0 ? (j + dy) % length : j + dy + length;
-                //var u = i + dx;
-                //var v = j + dy;
-                if (u >= 0 && u < width && v >= 0 && v < length && map[u * length + v])
+                var u = i + dx >= 0 ? (i + dx) % width : (i + dx + width) % width;
+                var v = j + dy >= 0 ? (j + dy) % length : (j + dy + length) % length;
+                if (map[u * length + v])
                 {
                     alives++;
                 }
@@ -106,7 +183,7 @@ class Program
             {
                 for(var j = 0; j < length; j++)
                 {
-                    if (lines[i][j] != ' ')
+                    if (lines[i][j] == '*')
                         map[i * length + j] = true;
                 }
             }
